e42fd81185512bf8d075e7369d09ae15c2a20894
AVISHEK DUTTA <109311355+dutta6t9@users.noreply.github.com>
2025-05-09 16:49:36 +0530

    Trapped Rain Water
    wrong comments are fixed

--- Java Programming/Java Basics/9. Arrays Part II/TRW.java
+++ Java Programming/Java Basics/9. Arrays Part II/TRW.java
@@ -2,12 +2,16 @@ public class TRW {
     public static void main(String args[]){
         int height[] ={4,2,0,6,3,2,5};
 
-        System.out.println(TrappedRainwater(height));
+        System.out.println();
+        System.out.print("Trapped water stored :");
+        System.out.print(TrappedRainwater(height));
+        System.out.println();
+        System.out.println();
     }
 
     public static int TrappedRainwater(int height[]){
         int n = height.length;
-        // Calculate rightmax -> Array ( Auxiliary Array)
+        // Calculate Leftmax -> Array ( Auxiliary Array)
         int leftmax[]=new int[n];
 
         leftmax[0] = height[0];
@@ -15,8 +19,8 @@ public class TRW {
             leftmax[i] = Math.max(height[i], leftmax[i-1]);
         }
 
-        //  Calculate RihtMax --> array
-
+        //  Calculate RihtMax --> array (Auxiliary Array)
+        //  Calculate Array from right side 
         int rightmax[] = new int[n];
         rightmax[n-1]=height[n-1];
         for(int i = n-2 ; i>= 0 ; i--){
@@ -28,7 +32,7 @@ public class TRW {
 
         int trappeRaindwater = 0 ;
         for (int i = 0 ; i < n ; i++){
-            // water level MINIMUM 
+            // water level MINIMUM calculate and initialize water level 
 
             int waterLevel = Math.min(leftmax[i], rightmax[i]);
 
