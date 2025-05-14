a123bca501a0957b28c541d81b41f948c6f8ccc1
AVISHEK DUTTA <109311355+dutta6t9@users.noreply.github.com>
2025-05-14 16:39:21 +0530

    Collections frame work
    work in functions

--- Java Programming/Java Basics/10. Sorting Algorithm/Inbuilt.java
+++ Java Programming/Java Basics/10. Sorting Algorithm/Inbuilt.java
@@ -5,7 +5,8 @@ public class Inbuilt {
         Integer arr[]={5,4,1,3,2};
         // Arrays.sort(arr);
         // Arrays.sort(arr,0,3);
-        Arrays.sort(arr,Collections.reverseOrder());
+        // Arrays.sort(arr,Collections.reverseOrder());
+        Arrays.sort(arr,0,4,Collections.reverseOrder());
         PrintArray(arr);
     }
     public static void PrintArray(Integer arr[]){
