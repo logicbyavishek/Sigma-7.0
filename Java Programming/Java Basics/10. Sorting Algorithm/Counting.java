15b7d4037d7aa7322b068e1fa5516f3a9636dbec
AVISHEK DUTTA <109311355+dutta6t9@users.noreply.github.com>
2025-05-15 02:21:57 +0530

    Counting Sort
    only for positive number .

---
+++ Java Programming/Java Basics/10. Sorting Algorithm/Counting.java
@@ -0,0 +1,34 @@
+public class Counting {
+    
+    public static void CountSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i =0 ; i<arr.length;i++){
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count [] = new int [largest +1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        // Sorting 
+        int j =0 ;
+        for(int i =0 ;i <count.length;i++){
+            while (count[i]>0) {
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+    
+    public static void PrintArray(int arr[]){
+        for(int i =0 ; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,4,1,3,2,4,3,7};
+        CountSort(arr);
+        PrintArray(arr);
+    }
+}
