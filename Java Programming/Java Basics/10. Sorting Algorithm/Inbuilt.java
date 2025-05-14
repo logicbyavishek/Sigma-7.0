6d042c0e942e1371b6ea8063162ec8a498b0e5e9
AVISHEK DUTTA <109311355+dutta6t9@users.noreply.github.com>
2025-05-14 16:22:02 +0530

    Inbuilt sort

---
+++ Java Programming/Java Basics/10. Sorting Algorithm/Inbuilt.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;;
+public class Inbuilt {
+    public static void main(String[] args) {
+        int arr[]={5,4,1,3,2};
+        Arrays.sort(arr);
+        PrintArray(arr);
+    }
+    public static void PrintArray(int arr[]){
+        for(int i = 0 ; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
