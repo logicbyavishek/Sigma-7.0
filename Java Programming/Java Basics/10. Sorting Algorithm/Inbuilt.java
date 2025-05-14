1eda5ac0bbd244cb663ab7b7ea36d09887e12425
AVISHEK DUTTA <109311355+dutta6t9@users.noreply.github.com>
2025-05-14 16:35:08 +0530

    Collectiopns Package use

--- Java Programming/Java Basics/10. Sorting Algorithm/Inbuilt.java
+++ Java Programming/Java Basics/10. Sorting Algorithm/Inbuilt.java
@@ -1,12 +1,14 @@
-import java.util.Arrays;;
+import java.util.*;
+import java.util.Collections;
 public class Inbuilt {
     public static void main(String[] args) {
-        int arr[]={5,4,1,3,2};
+        Integer arr[]={5,4,1,3,2};
         // Arrays.sort(arr);
-        Arrays.sort(arr,0,3);
+        // Arrays.sort(arr,0,3);
+        Arrays.sort(arr,Collections.reverseOrder());
         PrintArray(arr);
     }
-    public static void PrintArray(int arr[]){
+    public static void PrintArray(Integer arr[]){
         for(int i = 0 ; i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
