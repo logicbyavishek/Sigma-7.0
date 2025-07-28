c40d70b88f260343650538078f2af6e0c199aaa1
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-07-28 20:36:15 +0530

    Improve anagram check logic in anagrams.java
    Rewrote the checkAnagrams method to compare sorted character arrays of the input strings, making the check case-insensitive and accurate. Updated the main method to use a mixed-case test case.

--- Java Programming/Java Basics/12. Strings/anagrams.java
+++ Java Programming/Java Basics/12. Strings/anagrams.java
@@ -1,17 +1,33 @@
+import java.util.*;
+
 public class anagrams {
     public static boolean checkAnagrams(String str,String str1){
-        for(int i=0;i<str.length();i++){
-            for(int j=0;j<str1.length();j++){
-                if(str.charAt(i)==str1.charAt(j)){
-                    return true;
-                }
-            }
+        if(str.length()!=str1.length()){
+            return false;
+        }else{
+            // make all string into lower case if we dont use case sensitive
+            String Lostr,Lostr1;
+            Lostr = str.toLowerCase();
+            Lostr1 = str1.toLowerCase();
+            
+            // Lostr=str;Lostr1=str1; if we use case sensitive then use it
+
+            // create char arry
+            char [] arr1 = Lostr.toCharArray();
+            char [] arr2 = Lostr1.toCharArray();
+
+            // sort this arrays
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+
+            // check both are equal or not ?
+
+            return Arrays.equals(arr1,arr2);
         }
-        return false;
     }
     public static void main(String[] args) {
         String str ="care";
-        String str1 = "race";
+        String str1 = "RACE";
         System.out.println("both string are anagrams ? :"+checkAnagrams(str, str1));
     }
 }
