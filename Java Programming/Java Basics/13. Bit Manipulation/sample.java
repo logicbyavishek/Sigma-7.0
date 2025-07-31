4afd8f7c43e109bbb9ace976448566666ac5b5d9
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-07-31 20:23:41 +0530

    Bit Manupulation sample code boiler plate
    only do all the bit manipulation code in very basic way

---
+++ Java Programming/Java Basics/13. Bit Manipulation/sample.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class sample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // learn all about Bit Manipulation
+        // Binart And &
+        System.out.println(5&6);
+        // Binary or |
+        System.out.println(5|6);
+        // Binary XOR ^
+        System.out.println(5^6);
+        // Binary 1's Compliment
+        System.out.println(~5);
+        // Binary Left Shift <<
+        System.out.println(5<<2); // a<<b = a * 2^b
+        // Binary Right Shift
+        System.out.println(6>>1); // a>>b = a/2^b
+    }
+}
