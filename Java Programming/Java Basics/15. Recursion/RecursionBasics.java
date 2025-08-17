531429ef8a490854c220a92cc9f27e7b6351d700
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-08-17 06:21:55 +0530

    commit

--- Java Programming/Java Basics/15. Recursion/RecursionBasics.java
+++ Java Programming/Java Basics/15. Recursion/RecursionBasics.java
@@ -8,11 +8,18 @@ public class RecursionBasics {
         printDec(n-1);
     }
 
-
+    public static void printInc(int n){
+        if(n==1){
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n +" ");
+    }
 
     public static void main(String[] args) {
         int n = 10;
-        printDec(n);
-        
+        // printDec(n);
+        printInc(n);
     }
 }
