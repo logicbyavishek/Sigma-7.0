bd91cd2ceb6e2693879d994dc84419f6a6423246
logicbyavishek <109311355+logicbyavishek@users.noreply.github.com>
2025-08-18 10:27:19 +0530

    commit

--- Java Programming/Java Basics/15. Recursion/RecursionBasics.java
+++ Java Programming/Java Basics/15. Recursion/RecursionBasics.java
@@ -37,11 +37,21 @@ public class RecursionBasics {
         return fn ;
     }
 
+    public static int Sum(int n){
+        if(n==1){
+            return 1;
+        }
+        int fnm1= Sum(n-1);
+        int fn = n + Sum(n-1);  
+        return fn ;
+    }
+
     public static void main(String[] args) {
         int n = 5;
         // printDec(n);
         // printInc(n);
         // fact(n , 1);
-        System.out.println(facto(n));
+        // System.out.println(facto(n));
+        System.out.println(Sum(n));
     }
 }
